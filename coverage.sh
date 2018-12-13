8814261f1fb52da13682542048d8049bed1d8908
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2018-12-13 21:02:06 +0530

    Added coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -3,7 +3,7 @@ mkdir coverage
 dart --pause-isolates-on-exit --enable_asserts --enable-vm-service test/.test_coverage.dart &
 sleep 10
 pub run coverage:collect_coverage --uri=http://127.0.0.1:8181 -o coverage/coverage.json --resume-isolates
-pub run coverage:format_coverage -l --packages=/home/prijindal/projects/kuzzle_dart/.packages -i coverage/coverage.json --report-on=lib -o coverage/lcov.info
+pub run coverage:format_coverage -l --packages=$(pwd)/.packages -i coverage/coverage.json --report-on=lib -o coverage/lcov.info
 
 killall dart
 wait
